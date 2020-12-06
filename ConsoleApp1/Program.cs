bbc43e41d7ab3b1fc68be3cc9f8b1d892710e5a0
Allen Kuo <g01.webapp@gmail.com>
2020-12-06 13:24:09 +0800

    add:文字固定長度, 數字靠右對齊

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	/// <summary>
+	///  
+	/// </summary>
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] items = {"abc", "abcdefghijklm" };
+			foreach (var item in items)
+			{
+				string msg = $"{item}\t{item.Length}";
+				Console.WriteLine(msg);
+			}
+
+			Console.WriteLine("==================");
+
+			int maxLen = items.Select(s => s.Length).Max();
+			foreach (var item in items)
+			{
+				string msg = $"{item.PadRight(maxLen+1)}\t{item.Length,4}";
+				Console.WriteLine(msg);
+			}
+
+		}
+	}
+}
